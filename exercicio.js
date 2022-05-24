558a15a39cfec014c8ad63d29571a2c6e7aa8a21
purgatoryalt <102757720+purgatoryalt@users.noreply.github.com>
2022-05-24 13:15:57 -0300

    Add files via upload

---
+++ exercicio.js
@@ -0,0 +1,92 @@
+var mediaTotal = document.querySelector ('#primeiro-estudantes');
+
+var notaUm = mediaTotal.querySelector ('.info-html');
+var altura1 = parseInt(notaUm.textContent);
+console.log(altura1)
+
+var notaDois = mediaTotal.querySelector ('.info-css');
+var altura2 = parseInt(notaDois.textContent);
+console.log(altura2)
+
+var notaTres = mediaTotal.querySelector ('.info-js');
+var altura3 = parseInt(notaTres.textContent);
+console.log(altura3)
+
+
+var resultado = (altura1+altura2+altura3) / 3;
+
+var media = mediaTotal.querySelector ('.info-media')
+media.textContent = resultado 
+
+
+
+var mediaTotal = document.querySelector ('#seg-estudantes');
+var notaUm = mediaTotal.querySelector ('.info-html');
+var altura1 = parseInt(notaUm.textContent);
+console.log(altura1)
+
+var notaDois = mediaTotal.querySelector ('.info-css');
+var altura2 = parseInt(notaDois.textContent);
+console.log(altura2)
+
+var notaTres = mediaTotal.querySelector ('.info-js');
+var altura3 = parseInt(notaTres.textContent);
+console.log(altura3)
+
+var resultado = (altura1+altura2+altura3) / 3;
+var media = mediaTotal.querySelector ('.info-media')
+media.textContent = resultado 
+
+
+var mediaTotal = document.querySelector ('#ter-estudantes');
+var notaUm = mediaTotal.querySelector ('.info-html');
+var altura1 = parseInt(notaUm.textContent);
+console.log(altura1)
+
+var notaDois = mediaTotal.querySelector ('.info-css');
+var altura2 = parseInt(notaDois.textContent);
+console.log(altura2)
+
+var notaTres = mediaTotal.querySelector ('.info-js');
+var altura3 = parseInt(notaTres.textContent);
+console.log(altura3)
+
+var resultado = (altura1+altura2+altura3) / 3;
+var media = mediaTotal.querySelector ('.info-media')
+media.textContent = resultado 
+
+
+var mediaTotal = document.querySelector ('#qua-estudantes');
+var notaUm = mediaTotal.querySelector ('.info-html');
+var altura1 = parseInt(notaUm.textContent);
+console.log(altura1)
+
+var notaDois = mediaTotal.querySelector ('.info-css');
+var altura2 = parseInt(notaDois.textContent);
+console.log(altura2)
+
+var notaTres = mediaTotal.querySelector ('.info-js');
+var altura3 = parseInt(notaTres.textContent);
+console.log(altura3)
+
+var resultado = (altura1+altura2+altura3) / 3;
+var media = mediaTotal.querySelector ('.info-media')
+media.textContent = resultado 
+
+var mediaTotal = document.querySelector ('#quin-estudantes');
+var notaUm = mediaTotal.querySelector ('.info-html');
+var altura1 = parseInt(notaUm.textContent);
+console.log(altura1)
+
+var notaDois = mediaTotal.querySelector ('.info-css');
+var altura2 = parseInt(notaDois.textContent);
+console.log(altura2)
+
+var notaTres= mediaTotal.querySelector ('.info-js');
+var altura3 = parseInt(notaTres.textContent);
+console.log(altura3)
+
+var resultado = (altura1+altura2+altura3) / 3;
+var media = mediaTotal.querySelector ('.info-media')
+media.textContent = resultado 
+
